c3d904ddab2ba939638ff03f39c31f302cd119d8
JNeyNeto <122401966+jneyneto@users.noreply.github.com>
2023-08-24 07:54:48 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -7,12 +7,18 @@ function RotasProtegidas({ redirectTo }) {
   const token = localStorage.getItem("token");
 
   return token ? <Outlet /> : <Navigate to={redirectTo} />;
-}
+};
+
+function NotProtectedRoutes() {
+	const token = localStorage.getItem('token');
+
+	return token ? <Navigate to="/home" /> : <Outlet />
+};
 
 export default function RotasPrincipais() {
   return (
     <Routes>
-      <Route>
+      <Route element={<NotProtectedRoutes />}>
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/cadastro" element={<Cadastro />} />
